fab0d3d60abf7a4f23e9b70c7e82b7aaec2b4a98
TheKiruls <amirin856@gmail.com>
2024-05-24 12:15:48 +0800

    random password generator

---
+++ Random Password Generator/script.js
@@ -0,0 +1,31 @@
+const passwordBox = document.querySelector("#password");
+
+const lengthPass = 15;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase ="abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789"
+const symbol = "!@#$$%^&*(){}:_+=-[]|/,.`~";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(lengthPass > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    passwordBox.value = password;
+}
+
+function copyPassword () { 
+    passwordBox.select();
+    navigator.clipboard.writeText(passwordBox.value);
+}
+
+
