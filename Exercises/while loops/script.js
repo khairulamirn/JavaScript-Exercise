d10092d8be7c25f80d29939a7b347b5dd8f09ace
TheKiruls <amirin856@gmail.com>
2024-05-30 23:19:54 +0800

    while loop exercise p1

---
+++ Exercises/while loops/script.js
@@ -0,0 +1,51 @@
+//  https://www.youtube.com/watch?v=DfBCcyauAys
+
+// QUESTION 1
+// create a for loop that prints out the numbers from 5-10 (inclusive)
+
+let i = 5;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+// QUESTION 2
+// Create a for loop that will print out all the even numbers between 10 and 40.
+//  Do the same for all odd number as well.
+
+let a = 10;
+while (a <= 40) {
+    a++;
+    if (a % 2 === 0) {
+    console.log(a);
+    } else {
+    (a % 2 != 0);
+    console.log(a);
+    } 
+} 
+
+//  QUESTION 3 
+/*  
+    1. Create a variable called "loggedIn" that starts off as false.
+    2. Create a for loop that loops while "loggedIn" is false.
+    3. Each loop, print out: "Incorrect login credentials"
+    4. After 3 loop iterations, change "loogedIn" to be true. 
+    5. After the loops is done print out: "Successfully logged in!"
+
+    HINT1: This is more challenging with a for loop
+    HINT2: This is not an ideal candidate for a for loop
+    HINT3: You don't have to provide all parts of the for loop construct
+*/
+
+let loggedIn = false;
+let b = 0;
+
+while (!loggedIn) {
+    b++;
+    console.log("Incorrect login credentials");
+
+    if (b === 2) {
+        loggedIn = true;
+        console.log("Successfully logged in");
+    }
+}
